6ec1786bd6bd37cd94f6b5e80ddec5a8082744e4
LucasFCustodio <lcustodio2023@fau.edu>
2026-01-08 13:13:21 -0500

    Pipefy info received successfully in the app

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ import 'dotenv/config';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.text({ type: '*/*' }));
 
 app.get("/", (req, res) => {
     res.send("The port is working")
 });
 
 app.post("/solicitacao-estadual", (req, res) => {
-    const businessName = data["Business Name"];
-    const businessAddress = data["Business Address"];
-    console.log("Business name: " + businessName + ", Business Address: " + businessAddress);
-    //Store business name later
+    console.log("Raw Data received from Pipefy:", req.body);
 
     //Notify the server the response was successfully received
     res.status(200).send("HTTP request sucessfully received from Pipefy");
